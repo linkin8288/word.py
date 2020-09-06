024c4a0273f2b670a30bb981315b8d73dd490ff3
Charles <linkin8288@gmail.com>
2020-09-06 06:38:56 +0200

    modify template of third party kit

---
+++ word.py
@@ -0,0 +1,48 @@
+# 使用python-docx的套件說明，首先載入程式碼，但因為這個套件寫得沒有很好
+#，要先註解掉她的一些部分，才可以產生我們想要的模板。
+
+
+from docx import Document
+from docx.shared import Inches
+
+document = Document()
+
+document.add_heading('這是python操作word檔的範例', 0)
+
+p = document.add_paragraph('A plain paragraph having some ')
+p.add_run('bold').bold = True
+p.add_run(' and some ')
+p.add_run('italic.').italic = True
+
+document.add_heading('Heading, level 1', level=1)
+document.add_paragraph('Intense quote', style='Intense Quote')
+
+document.add_paragraph(
+    'first item in unordered list', style='List Bullet'
+)
+document.add_paragraph(
+    'first item in ordered list', style='List Number'
+)
+
+# document.add_picture('monty-truth.png', width=Inches(1.25))
+
+records = (
+    (3, '101', 'Spam'),
+    (7, '422', 'Eggs'),
+    (4, '631', 'Spam, spam, eggs, and spam')
+)
+
+# table = document.add_table(rows=1, cols=3)
+# hdr_cells = table.rows[0].cells
+# hdr_cells[0].text = 'Qty'
+# hdr_cells[1].text = 'Id'
+# hdr_cells[2].text = 'Desc'
+# for qty, id, desc in records:
+#     row_cells = table.add_row().cells
+#     row_cells[0].text = str(qty)
+#     row_cells[1].text = id
+#     row_cells[2].text = desc
+
+document.add_page_break()
+
+document.save('demo.docx')
